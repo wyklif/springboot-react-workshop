546a05b642204e8d896ae3447250a0c9f21d8e26
Wycliffe <ottawawycliffe@gmail.com>
2025-10-06 23:07:16 +0300

    initia;

---
+++ src/main/java/com/example/Springboot_React_Workshop/SpringbootReactWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_React_Workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springboot_React_Workshop/SpringbootReactWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_React_Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
