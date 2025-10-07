e6a290f14df6be1774cd6e973a65ec7e09a015c6
Wycliffe <ottawawycliffe@gmail.com>
2025-10-07 23:25:04 +0300

    created mapper for employee class and Dto

---
+++ src/main/java/com/example/Springboot_React_Workshop/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.example.Springboot_React_Workshop.mapper;
+
+import com.example.Springboot_React_Workshop.dto.EmployeeDto;
+import com.example.Springboot_React_Workshop.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
